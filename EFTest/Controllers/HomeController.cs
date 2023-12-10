8bd1b40ebbbc06c30ad0b81db508f44c63944397
sya.sya <prashantmessi08@gmail.com>
2023-12-10 14:01:06 +0545

    Index Layout > LogIn | UIUX | SHINIGAMI

--- EFTest/Controllers/HomeController.cs
+++ EFTest/Controllers/HomeController.cs
@@ -31,12 +31,5 @@ namespace EFTest.Controllers
             }
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
