2a74fa967617068c034552642015eddfcb63fa60
sya.sya <prashantmessi08@gmail.com>
2023-12-24 16:14:52 +0545

    ???

--- EFTest/Controllers/LogInController.cs
+++ EFTest/Controllers/LogInController.cs
@@ -1,4 +1,5 @@
 ﻿using EFTest.Models;
+using EFTest.Models.ForgotPassword;
 using System.Web.Mvc;
 
 namespace EFTest.Controllers
@@ -6,6 +7,7 @@ namespace EFTest.Controllers
     public class LogInController : Controller
     {
         // GET: LogIn
+        #region Log In
         [HttpGet]
         public ActionResult Index()
         {
@@ -14,11 +16,69 @@ namespace EFTest.Controllers
         [HttpPost]
         public ActionResult Index(UserModel model)
         {
+            ModelState.Remove("Email");
+            ModelState.Remove("MobileNumber");
             if (ModelState.IsValid)
             {
-                return RedirectToAction("Index","Home");
+                return RedirectToAction("Index", "Home");
             }
             return View();
         }
+        #endregion
+
+        #region Register
+        [HttpPost]
+        public ActionResult Register(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "LogIn");
+            }
+            return RedirectToAction("Index", "LogIn");
+        }
+        #endregion
+
+        #region Forgot Password
+        [HttpGet]
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ForgotPassword(ForgotPassword model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("SendOTP", "LogIn");
+            }
+            return View();
+        }
+        #endregion
+
+        #region Send OTP
+        [HttpGet]
+        public ActionResult SendOTP()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendOTP(OTPCode model)
+        {
+            return RedirectToAction("PasswordReset", "LogIn");
+        }
+        #endregion
+
+        #region Reset Password
+        [HttpGet]
+        public ActionResult PasswordReset()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PasswordReset(ResetPassword model)
+        {
+            return RedirectToAction("Index", "LogIn");
+        }
+        #endregion
     }
 }
